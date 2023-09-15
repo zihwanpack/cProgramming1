e06b58e0b904898d62d2b979d417d0049cdb91fd
zihwanpack <jihwan8807@naver.com>
2023-09-15 23:58:28 +0900

    firstAssignment

---
+++ fistAssign.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    int num1, num2;
+
+    printf("Input two integers: ");
+    scanf("%d %d", &num1, &num2);
+    
+    printf("%d & %d = %d \n", num1, num2, num1 & num2);
+    printf("%d | %d = %d \n", num1, num2, num1 | num2);
+    printf("%d ^ %d = %d \n", num1, num2, num1 ^ num2);
+
+    return 0;
+};
