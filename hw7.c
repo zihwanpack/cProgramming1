87df013d481bc6b6f07e7fe8c574fe8b907629b4
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2023-11-23 22:40:06 +0900

    hw8.c

--- hw7.c
+++ hw7.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+void printArr(int arr[]){
+  for(int j = 0; j < 6; j++){
+    printf("%d ", arr[j]);
+  }
+}
+
 int main(void) {
   int arr1[6] = {1, 2, 3, 4, 5, 6};
   int arr2[6] = {7, 8, 9, 10, 11, 12};
@@ -8,14 +14,10 @@ int main(void) {
   int tmp;
 
   printf("arr1: ");
-  for(int j = 0; j < 6; j++) {
-    printf("%d ", arr1[j]);
-  }
+  printArr(arr1);
   
   printf("\narr2: ");
-  for(int j = 0; j < 6; j++) {
-    printf("%d ", arr2[j]);
-  }
+  printArr(arr2);
 
   for(int i = 0; i < 6; i++) {
     tmp = *ptr1;
@@ -26,13 +28,9 @@ int main(void) {
   }
 
   printf("\n\nafter swap\narr1: ");
-  for(int j = 0; j < 6; j++) {
-    printf("%d ", arr1[j]);
-  }
+  printArr(arr1);
 
   printf("\narr2: ");
-  for(int j = 0; j < 6; j++) {
-    printf("%d ", arr2[j]);
-  }
+  printArr(arr2);
 
 }
---
+++ hw8.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+double myPow(double a, double b) {
+    return pow(a, b);
+}
+
+
+double mySqrt(double x) {
+    return sqrt(x);
+}
+
+double calculate(int arr[], int n) {
+    double sum = 0, aver, num = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    aver = sum / n;
+
+    for (int i = 0; i < n; i++) {
+        num += myPow(arr[i] - aver, 2);
+    }
+
+    return mySqrt(num / n);
+}
+
+int main() {
+    int arr[5];
+    
+    printf("Enter 5 real arr: ");
+    for (int i = 0; i < 5; ++i) {
+        scanf("%d", &arr[i]);
+    }
+
+    double result = calculate(arr, 5);
+    printf("Standard Deviation = %.3lf\n", result);
+
+    return 0;
+}
